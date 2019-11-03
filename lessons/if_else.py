33b6ff37e7a30bfdcf855d4ba99aac8cc2b07d4b
yasminaElg <leyayasmina@gmail.com>
2019-11-03 19:00:02 +0100

    if elif else conditions for and while loops

---
+++ lessons/if_else.py
@@ -0,0 +1,17 @@
+
+
+# age = int(input('Enter your age: '))
+#
+# if age < 10:
+#     print('you are young strange one')
+# elif age < 40:
+#     print('the fire in you is strong')
+# else:
+#     print('you are wise beyond doubt')
+
+meaty = input('Do you eat meet? (y/n)')
+
+if meaty == 'y':
+    print('here is the meaty menu....')
+else:
+    print('here is the veggie menu....')
