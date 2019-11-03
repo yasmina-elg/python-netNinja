6bd0f65633513b19bff0c5b5607dc9e019363170
yasminaElg <leyayasmina@gmail.com>
2019-11-03 18:26:32 +0100

    lesson on formating strings

---
+++ lessons/string_format.py
@@ -0,0 +1,13 @@
+num1 = 3.1425467389
+num2 = 10.2903948
+
+# PREVIOUS WAY
+#print('num 1 is:', num1, 'and num 2 is', num2)
+
+# FORMAT METHOD
+
+#print('num 1 is {0:.3f} and num 2 is {1:.3f}'.format(num1, num2))
+
+# USING F-STRINGS
+
+print(f'num 1 is {num1:.3f} and num 2 is {num2:.3f}')
