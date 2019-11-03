66f2627b638d01ccfae43220b6070dfc0c67822a
yasminaElg <leyayasmina@gmail.com>
2019-11-03 18:10:56 +0100

    first python programme that calculates the area of a circle

---
+++ area_calc.py
@@ -0,0 +1,9 @@
+# name = input('Teel me your name punk: ')
+# age = input('...and your age: ')
+# print(name, 'you are', age)
+
+#calc the area of a circle
+
+radius = input('Enter the radius of your circle(m): ')
+area = 3.142 * int(radius)**2
+print('The area of your circle is:', area)
